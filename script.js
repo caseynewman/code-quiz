7524497e63bfeac95890c2775f4975a1d881e20e
Yogi Spice <caseynnewman@yahoo.com>
2023-12-12 12:11:16 -0500

    adding html

---
+++ script.js
@@ -0,0 +1,10 @@
+// store high scores
+// set a timer - decrement time every time the answer is incorrect
+// store correct answers
+// store incorrect answers
+// 
+
+// highlight green when correct
+// highlight red when incorrect
+
+// high scores & timer in flexbox?
